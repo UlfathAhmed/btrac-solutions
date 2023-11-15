9dd99e94a6686a9a680dd66609cc7d41b48b9198
UlfathAhmed <atosahmed2015@gmail.com>
2023-11-15 13:47:12 +0600

    commit massage

--- src/pages/ServicePage/partials/ServiceCard.jsx
+++ src/pages/ServicePage/partials/ServiceCard.jsx
@@ -17,7 +17,7 @@ const ServiceCard = () => {
         {ServiceData.map((item) => (       
             <div className=" relative border-2 border-black dark:border-[#414141]  group hover:bg-[#225FA9] duration-500 h-[32rem] flex flex-col justify-center items-center" >
                 <h1 className='font-blackerdisplay font-normal dark:text-white text-4xl rotate-90 text-center cursor-pointer group-hover:rotate-0 duration-300 group-hover:mb-12'>{item.title}</h1>
-                <p className='font-syne font-normal text-xl dark:text-white text-center px-2 hidden group-hover:block '>{item.description}</p>
+                <p className='font-syne font-normal text-xl dark:text-white text-center px-4 hidden group-hover:block '>{item.description}</p>
                 <button>
                     <img src={item.image} className='absolute bottom-5 left-28  rotate-0 group-hover:rotate-45 duration-300'/>
                 </button>
