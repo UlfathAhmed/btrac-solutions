28044553a5c9371c3d8ebcad1997ed76d1f55b3f
UlfathAhmed <atosahmed2015@gmail.com>
2024-01-02 16:38:08 +0600

    project page

--- src/DummyData/ProjectsData.jsx
+++ src/DummyData/ProjectsData.jsx
@@ -7,35 +7,46 @@ export const ProjectsData = [
   {
     id: 1,
     image: product1,
-    heading: "APPLICATION DEVELOPMENT AND MAINTENANCE",
-    subHeading: "Transforming Your Business with Tailored Applications",
-    description:
-      "Our skilled developers create customized applications tailored to your unique business needs. We also provide application maintenance services to ensure smooth system functionality, allowing you to concentrate on core business activities.",
+    heading: "Project Name",
+    
+   
   },
   {
     id: 2,
     image: product2,
-    heading: "IT INFRASTRUCTURE ENGINEERING",
-    subHeading: "Engineering Your Success with Reliable IT Infrastructure",
-    description:
-      "We offer end-to-end IT infrastructure engineering services to design, deploy, and maintain enterprise-grade IT systems. Our technical expertise ensures that we provide our clients with scalable and future-proof IT solutions.",
+    heading: "Project Name",
+    
+   
   },
   {
     id: 3,
     image: product3,
-    heading: "SYSTEM INTEGRATION",
-    subHeading: "Seamless Integration for Optimal Performance",
-    description:
-      "We specialize in seamlessly integrating disparate systems to create a unified IT infrastructure and minimize disruption to day-to-day operations. Our team of experts is equipped to successfully integrate complex systems and optimize their performance.",
+    heading: "Project Name",
+    
+   
   },
   {
     id: 4,
     image: product1,
-    heading: "CLOUD SERVICES",
-    subHeading: "Elevating Your Business to New Heights with Cloud Solutions",
-    description:
-      "Our secure and scalable cloud solutions help our clients migrate their IT infrastructure to the cloud to help businesses to improve flexibility, reduce costs, and enhance scalability. We also offer ongoing cloud management and optimization services to ensure that our clients get the most out of their cloud-based systems.",
+    heading: "Project Name",
+    
+   
   },
+  {
+    id: 5,
+    image: product2,
+    heading: "Project Name",
+    
+   
+  },
+  {
+    id: 6,
+    image: product3,
+    heading: "Project Name",
+    
+   
+  },
+
  
 ];
 
